cffff34231d0483c0bbeffd9cbb076441ddcf837
shadab <shadab14meb346@gmail.com>
2023-03-09 00:55:28 +0530

    minor

--- src/components/Sidebar/useStyles.tsx
+++ src/components/Sidebar/useStyles.tsx
@@ -4,7 +4,7 @@ export const useStyles = makeStyles((theme) => ({
 	drawerPaper: {
 		width: "50%",
 		background: theme.palette.primary.light,
-		height: "40%",
+		height: "50%",
 	},
 	paperAnchorRight: {
 		top: 72,
